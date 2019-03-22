944500dbcf67232a52974ebb1f38e75bec53ddb5
Catherine Bronzo <cbronzo@mac.com>
2019-03-22 04:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,27 +2,27 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 
-let neighborhoodId = 0;
-let mealId = 0;
-let customerId = 0;
-let deliveryId = 0;
+let neighborhoodId = 0
+let mealId = 0
+let customerId = 0
+let deliveryId = 0
 
  class Neighborhood {
   constructor(name){
     this.name = name
     this.id = ++neighborhoodId
-    store.neighborhoods.push(this);
+    store.neighborhoods.push(this)
   }
 
    deliveries() {
       return store.deliveries.filter(delivery => {
-              return delivery.neighborhoodId == this.id;
+              return delivery.neighborhoodId == this.id
           }
       );
   }
   customers() {
       return store.customers.filter(customer => {
-              return customer.neighborhoodId == this.id;
+              return customer.neighborhoodId == this.id
           }
       );
   }
